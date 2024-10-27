467f684801eb8a04d161a44b104e8fcfed97986f
kotlovchanin <toguedriver@gmail.com>
2024-10-27 15:15:22 +0300

    первый коммит в другую ветку

---
+++ js_objects_training/objects.js
@@ -0,0 +1,57 @@
+// // ----- 1 ----- // введение
+// let weekdayNames = {
+//     1: 'Понедельник',
+//     2: 'Вторник',
+//     3: 'Среда',
+//     4: 'Четверг',
+//     5: 'Пятница',
+//     6: 'Суббота',
+//     7: 'Воскресенье'
+// }
+
+// // выводим все элементы объекта (функция)
+// function allElementsInObject(object) {
+//     for (let i = 1; i <= Object.keys(object).length; i++) [
+//         console.log(object[i])
+//     ]
+// }
+
+// allElementsInObject(weekdayNames);
+
+
+// // ----- 2 -----
+// let monthNames = {
+//     1: 'Январь',
+//     2: 'Февраль',
+//     3: 'Март',
+//     4: 'Апрель',
+//     5: 'Май',
+//     6: 'Июнь',
+//     7: 'Июль',
+//     8: 'Август',
+//     9: 'Сентябрь',
+//     10: 'Октябрь',
+//     11: 'Ноябрь',
+//     12: 'Декабрь'
+// }
+
+// // выводим в консоль весь объкт
+// console.log(monthNames);
+
+
+// ----- 3 -----
+function runCode() {
+    let user = {
+        name: 'Иван',
+        surname: 'Семенов',
+        patronymic: 'Иванович'
+    }
+
+    console.log(`${user['name']} ${user['surname']} ${user['patronymic']}`);
+    
+    alert(`${user['name']} ${user['surname']} ${user['patronymic']}`);
+}
+
+
+// ----- 4 -----
+
