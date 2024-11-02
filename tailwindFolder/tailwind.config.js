0cad1482d3f9f117c2abe2c0a28d0fb868f4bbd6
sharaga_work <toguedriver@gmail.com>
2024-11-02 10:28:25 +0300

    добавление папкм tailwindFolder с проектом использующим  tailwindcss

---
+++ tailwindFolder/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/css/*.css",
+    "./src/js/*.js",
+    "./src/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
