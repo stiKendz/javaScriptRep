80e37748ab5a39fbefbc91f362ba0d4639750ba6
sharaga <toguedriver@gmail.com>
2024-11-12 12:35:04 +0300

    добавилв в equation_roots приверку на 0 и немного подредактировал выражения для rootOne and rootTwo

--- equation_roots/index.js
+++ equation_roots/index.js
@@ -9,19 +9,25 @@ function calculateRoots() {
     let rootOne = 0;
     let rootTwo = 0;
 
-    disciminant = (bDigit.value ** 2) - 4 * aDigit.value * cDigit.value;
+    let a = Number(aDigit.value);
+    let b = Number(bDigit.value);
+    let c = Number(cDigit.value);
 
-    if (disciminant < 0) {
-        return outputResult.innerHTML = "Корней нет";
-    } else if (disciminant === 0) {
-        rootOne = -bDigit.value / (2 * aDigit.value);
-        return outputResult.innerHTML = `Уравнение имеет один корень ${rootOne}`; 
-    } else if (disciminant > 0) {
-        rootOne = -bDigit.value - Math.sqrt(disciminant) / (2 * aDigit.value);
-        rootTwo = -bDigit.value + Math.sqrt(disciminant) / (2 * aDigit.value);
-        return outputResult.innerHTML = `Уравнение имеет 2 корня ${rootOne}, ${rootTwo}`;
+    if (a === 0 || b === 0 || c === 0){
+        return outputResult.innerHTML = "Ошибка, в поле ввода не может быть 0";
     } else {
-        return outputResult.innerHTML = "Ошибка";
+        disciminant = (bDigit.value ** 2) - 4 * aDigit.value * cDigit.value;
+
+        if (disciminant < 0) {
+            return outputResult.innerHTML = "Корней нет";
+        } else if (disciminant === 0) {
+            rootOne = -bDigit.value / (2 * aDigit.value);
+            return outputResult.innerHTML = `Уравнение имеет один корень ${rootOne}`; 
+        } else if (disciminant > 0) {
+            rootOne = (-bDigit.value - Math.sqrt(disciminant)) / (2 * aDigit.value);
+            rootTwo = (-bDigit.value + Math.sqrt(disciminant)) / (2 * aDigit.value);
+            return outputResult.innerHTML = `Уравнение имеет 2 корня ${rootOne}, ${rootTwo}`;
+        } 
     }
 }
 
